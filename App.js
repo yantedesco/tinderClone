efca62332f1cfb2446644e0dd31f08e58246056e
Yan <yantedesco@gmail.com>
2021-07-07 08:14:54 -0300

    nao foi no add de ontem e so vi agora

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Home } from "./src/Home";
+import { Login } from "./src/Login";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<>
+			<StatusBar style="light" backgroundColor="#000" />
+			<Home />
+			{/* <Login /> */}
+		</>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
